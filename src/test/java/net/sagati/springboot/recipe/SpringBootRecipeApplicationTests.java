f268fbfd0aa09d13982d523b9bf85fe63dba1aa0
Manjunath <manjum_kar@yahoo.co.in>
2020-03-13 21:49:46 -0500

    Initial Commit

---
+++ src/test/java/net/sagati/springboot/recipe/SpringBootRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sagati.springboot.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
